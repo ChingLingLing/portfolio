2f4391a729a35d1462a123fa72b1669c370d000c
ChingLingLing <49142537+ChingLingLing@users.noreply.github.com>
2020-12-08 15:26:23 +0800

    start

---
+++ src/client.js
@@ -0,0 +1,8 @@
+//Purpose of this file is to communicate with sanity
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+    projectId:"0v79zanv",
+    dataset: "production"
+
+})
